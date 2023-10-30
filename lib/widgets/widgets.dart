d1a61c1fcabaeaca24b601ecfafb8dfbb94317f5
Nadia Disla Tejada <tejadanadiam@gmail.com>
2023-10-29 22:04:04 -0400

    Remove unused file.

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -4,7 +4,6 @@ export 'book_button_info_widget.dart';
 export 'book_detail_widget.dart';
 export 'book_image_widget.dart';
 export 'book_search_widget.dart';
-export 'books_list_widget.dart';
 export 'bottom_navigation_bar_button_widget.dart';
 export 'bottom_navigation_bar_widget.dart';
 export 'empty_results_widget.dart';
